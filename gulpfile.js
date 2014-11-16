03b821b0f9d6c64741c87152b4e31185d37bbddf
Danny Giebe <hello@creative-px.com>
2014-11-16 19:41:08 +0100

    browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
 		uglify = require('gulp-uglify'),
 		concat = require('gulp-concat'),
-		sass = require('gulp-ruby-sass');
+		sass = require('gulp-ruby-sass'),
+		browserSync = require('browser-sync'),
+		reload      = browserSync.reload;
 
 
 gulp.task('scripts', function() {
@@ -16,13 +18,29 @@ gulp.task('styles', function() {
     .pipe(sass({
     	style: 'compressed'
     }))
-    .pipe(gulp.dest('build/css/'));
+    .pipe(gulp.dest('build/css/'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('move', function() {   //Moves basic files to build folder
+    return gulp.src('./*.html')  //Change file ending, depending on files
+      .pipe(gulp.dest('build'));
 });
 
 //Watches JS
 gulp.task('watch', function() {
-    gulp.watch('src/js/*.js', ['scripts']);
+    gulp.watch('src/js/*.js', ['scripts', browserSync.reload]);
+    gulp.watch('src/sass/sass/*.sass', ['styles']);
+    gulp.watch('./*.html', ['move', browserSync.reload]);
 });
 
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+			    baseDir: "./",
+			    index: "index.html"
+				}
+    });
+});
 
-gulp.task('default',['scripts', 'watch']);
+gulp.task('default',['scripts', 'styles', 'browser-sync', 'move', 'watch']);
