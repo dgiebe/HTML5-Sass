8ee89aa368ae9c9b932ad02ae11b1b0418641764
Danny Giebe <hello@creative-px.com>
2014-11-16 16:04:47 +0100

    Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,28 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
+var gulp = require('gulp'),
+		uglify = require('gulp-uglify'),
+		concat = require('gulp-concat'),
+		sass = require('gulp-ruby-sass');
 
 
-gulp.task('default', function() {
+gulp.task('scripts', function() {
   return gulp.src('src/js/*.js')
 	  .pipe(concat('js/app.min.js'))
 	  .pipe(uglify())
 	  .pipe(gulp.dest('build'));
 });
+
+gulp.task('styles', function() {
+    return gulp.src('src/sass/style.sass')
+    .pipe(sass({
+    	style: 'compressed'
+    }))
+    .pipe(gulp.dest('build/css/'));
+});
+
+//Watches JS
+gulp.task('watch', function() {
+    gulp.watch('src/js/*.js', ['scripts']);
+});
+
+
+gulp.task('default',['scripts', 'watch']);
